e208c0af5013c7d3d6cdc13a3cfd0dc671eb11c9
Alex Glikson <alex.glikson@gmail.com>
2018-11-14 02:14:16 +0000

    Simplify inference flow

--- dogcat/inference.py
+++ dogcat/inference.py
@@ -2,13 +2,13 @@ import numpy as np
 import tensorflow as tf
 import time
 import h5py # We don't call this directly but needed for keras
-import argparse
 import random
 import pprint
 import operator
 
 import os
 import sys
+import shutil
 import tempfile
 import urllib
 
@@ -27,47 +27,25 @@ def get_model_input_shape(model):
     _, image_height, image_width, image_channel = model_input.shape
     return image_height, image_width, image_channel
 
-def create_inference_model(
-        model_spec_filepath,
-        random_seed,
-        verbose,
-        ):
+def create_inference_model(verbose):
     """
     Constructor for inference model
     """
     logger.info("Initializing the model...")
-    if random_seed is not None:
-        random.seed(random_seed)
-        np.random.seed(random_seed)
-        tf.set_random_seed(random_seed)
-    inf_model = tf.keras.models.load_model(model_spec_filepath)
+    inf_model = tf.keras.models.load_model('/tmp/model.hdf5')
     logger.info("...done")
     if verbose:
         inf_model.summary()
     return inf_model
 
-def run_inference_model(
-        inf_model,
-        batch_size,
-        image_width,
-        image_height,
-        inference_path,
-        choose_n_from_inference,
-        verbose,
-        auto_resize_images):
+def run_inference_model( inf_model, inference_path, verbose ):
     """
     Runs model training
     """
     logger.info("Loading images")
-    if auto_resize_images:
-        image_height, image_width, _ = get_model_input_shape(inf_model)
+    image_height, image_width, _ = get_model_input_shape(inf_model)
 
     inference_path_files = gen_util.get_all_files_in_dir(inference_path)
-    if choose_n_from_inference is not None:
-        inference_path_files = np.random.choice(
-                inference_path_files,
-                choose_n_from_inference,
-                replace=False)
     inference_path_filenames = list(map(lambda x : x.as_posix(), inference_path_files))
     inference_X = gen_util.load_images(
             path_files=inference_path_files,
@@ -75,7 +53,7 @@ def run_inference_model(
             image_height=image_height,
             )
     logger.info("Calculating prediction...")
-    train_y = inf_model.predict(inference_X, batch_size=batch_size, verbose=1)
+    train_y = inf_model.predict(inference_X, batch_size=32, verbose=1)
     logger.info("...done")
 
     file_to_pred = [(f.name, p[0]) for f, p in zip(inference_path_files, train_y.tolist())]
@@ -85,46 +63,6 @@ def run_inference_model(
 
 initialized = False
 inf_model = None
-used_model_spec_filepath = None
-
-def inference_model(
-    batch_size,
-    image_width,
-    image_height,
-    inference_path,
-    model_spec_filepath,
-    choose_n_from_inference,
-    random_seed,
-    verbose,
-    auto_resize_images):
-    """
-    Creates model lazily and runs model training
-    """
-    global initialized
-    global inf_model
-    global used_model_spec_filepath
-
-    if not initialized or used_model_spec_filepath != model_spec_filepath:
-        inf_model = create_inference_model(model_spec_filepath,
-                                           random_seed,
-                                           verbose)
-        initialized = True
-        used_model_spec_filepath = model_spec_filepath
-
-    sorted_file_to_pred = run_inference_model(
-            inf_model,
-            batch_size,
-            image_width,
-            image_height,
-            inference_path,
-            choose_n_from_inference,
-            verbose,
-            auto_resize_images)
-
-    return sorted_file_to_pred
-
-
-logger = gen_util.get_logger()
 
 def predict(params):
 
@@ -147,7 +85,7 @@ def predict(params):
 
     image_url = random.choice(random_images)
     images_url = None
-    tmp_dir = tempfile.mkdtemp() #TODO cleanup
+    tmp_dir = tempfile.mkdtemp()
     if params is not None:
         if 'image' in params:
             image_url = params['image']
@@ -162,26 +100,17 @@ def predict(params):
         filename = image_url[image_url.rfind("/")+1:]
         local_image_path,_ = urllib.urlretrieve(image_url,os.path.join(tmp_dir,filename))
         logger.info("Retrieved image file: '{}' --> '{}'.".format(image_url,local_image_path))
-    inf_dir = tmp_dir
 
-    model_url = 'https://s3.amazonaws.com/glikson-public/DLL/model/model_16x16.hdf5'
-    if 'MODEL' in os.environ:
-        model_url = os.environ['MODEL']
-    model_local_path = '/tmp/model.hdf5'
-    if not os.path.isfile(model_local_path):
-        logger.info("Downloading model '{}'".format(model_url))
-        urllib.urlretrieve(model_url, model_local_path)
-        logger.info("...done")
-    else:
-        logger.info("Using existing model file: '{}'".format(model_local_path))
+    global initialized
+    global inf_model
 
-    inf_res = inference_model(
-           batch_size = 32,
-           image_width = 16, image_height = 16, auto_resize_images = True,
-           inference_path = inf_dir,
-           model_spec_filepath = model_local_path,
-           choose_n_from_inference = None, random_seed = None,
-           verbose = False
-           )
+    if not initialized:
+        inf_model = create_inference_model(verbose=False)
+        initialized = True
+
+    inf_res = run_inference_model( inf_model, inference_path=tmp_dir, verbose=False)
+
+    shutil.rmtree(tmp_dir)
+
     return image_url, inf_res
 
